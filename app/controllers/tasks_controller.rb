cbfa1df561d97d665c122e9c06ffbb054bb73f4b
Sarah Garlock <garlock.sarah10@gmail.com>
2023-10-16 11:45:40 -0600

    add create task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,18 @@ class TasksController < ApplicationController
   def index
     @tasks = ["Task 1", "Task 2", "Task 3"]
   end
+
+  def new
+  end
+
+  def create
+    task = Task.new({
+      title: params[:title],
+      description: params[:description]
+    })
+
+    task.save
+
+    redirect_to "/tasks"
+  end
 end
