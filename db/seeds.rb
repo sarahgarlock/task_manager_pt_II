a407eefdeaf5714998d48151d10a62abfb39ef95
Sarah Garlock <garlock.sarah10@gmail.com>
2023-10-16 13:07:51 -0600

    add a seeded Task

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+task_4 = Task.create!(title: "Task Seed", description: "This is a seeded Task")
